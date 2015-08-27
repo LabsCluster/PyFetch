12010e99c740772827d32c0dcf660840b0247777
王万霖 <dgideas@outlook.com>
2015-08-28 00:03:26 +0800

    Update pyfetch.py

--- pyfetch.py
+++ pyfetch.py
@@ -5,3 +5,8 @@ def fetch(website,outfile,useragent="Links (2.8; Linux 2.6.32-504.30.3.el6.x86_6
 	subprocess.check_output('wget --user-agent="'+str(useragent)+'" --output-document='+str(outfile)+' -q "'+str(website)+'"',shell=True);
 	return;
 
+def fetchA(website,outfile,user,password,useragent="Links (2.8; Linux 2.6.32-504.30.3.el6.x86_64 x86_64; GNU C 4.4.7; text)"):
+	import subprocess;
+	subprocess.check_output('wget --user='+str(user)+' --password='+str(password)+' --user-agent="'+str(useragent)+'" --output-document='+str(outfile)+' -q "'+str(website)+'"',shell=True);
+	return;
+	
